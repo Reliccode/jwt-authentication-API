70c649e3ab5075a1c76e584c1e2980e1b2a7d85e
ExitMirth <maritimglen@gmail.com>
2024-03-27 22:26:32 +0300

    Automatic database table migration

--- initializers/syncDatabase.go
+++ initializers/syncDatabase.go
@@ -4,7 +4,7 @@ import (
 	"github.com/Reliccode/jwt-authentication-API/models"
 )
 
-// SyncDatabase performs automatic migration of database tables.
+// SyncDatabase performs migration of database tables automatically.
 // It ensures that the database schema matches the structure of the defined models.
 func SyncDatabase() {
 	DB.AutoMigrate(&models.User{})
