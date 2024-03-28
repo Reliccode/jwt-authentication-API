e5b4266c25639b1fdc3983929e5ff93cd094a40c
ExitMirth <maritimglen@gmail.com>
2024-03-29 00:50:57 +0300

    user struct models

--- models/userModel.go
+++ models/userModel.go
@@ -3,7 +3,7 @@ package models
 import "gorm.io/gorm"
 
 // User represents the user model in the application.
-// Structure definition of user data stored in the database.
+// Structure definitin of user data stored in the database.
 type User struct {
 	gorm.Model // gorm.Model is embedded to provide common fields like ID, CreatedAt, UpdatedAt, and DeletedAt
 	Email           string `gorm:"unique"` // Email field represents the email address of the user and is marked as unique in the database
